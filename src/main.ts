c9d8f5fa9e733d30211a269ac25a58215af750f6
Anthony Shaw <anthonyshaw@apache.org>
2019-07-25 09:15:03 +1000

    start using react components

--- src/main.ts
+++ src/main.ts
@@ -78,3 +78,4 @@ VSS.ready(function() {
         );
     });
 });
+
---
+++ src/ui/card.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Card } from "azure-devops-ui/Card";
+
+export default class CardExample extends React.Component {
+    public render(): JSX.Element {
+        return (
+            <Card className="flex-grow">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </Card>
+        );
+    }
+}
